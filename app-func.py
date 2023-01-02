f0d2c03f8b55fef45ecc5548f909182cfbe55039
XantyCtd <alexandrechotard@hotmail.fr>
2023-01-02 15:34:11 +0100

    nouveau modèle

--- app-func.py
+++ app-func.py
@@ -10,48 +10,52 @@ model = tf.keras.models.load_model("model.hdf5")
 img_height = 180
 img_width = 180
 
-@app.route('/')
-def index():
-    return render_template('upload.html')
 
-@app.route('/upload', methods=['POST'])
+@app.route("/")
+def index():
+    return render_template("upload.html")
+
+
+@app.route("/upload", methods=["POST"])
 def upload():
-    file = request.files['image']
+    file = request.files["image"]
     if file:
         # save the image to disk
-        file.save('image.jpg')
+        file.save("image.jpg")
 
         # read the image and treat it using OpenCV
-        img = tf.keras.utils.load_img('image.jpg', target_size=(img_height, img_width))
+        img = tf.keras.utils.load_img("image.jpg", target_size=(img_height, img_width))
         img_array = tf.keras.utils.img_to_array(img)
         img_array = tf.expand_dims(img_array, 0)  # Create a batch
 
         predictions = model.predict(img_array)
         score = tf.nn.softmax(predictions[0])
-        
+
         # save the treated image to disk
         class_names = [
-        "Bean",
-        "Bitter_Gourd",
-        "Bottle_Gourd",
-        "Brinjal",
-        "Broccoli",
-        "Cabbage",
-        "Capsicum",
-        "Carrot",
-        "Cauliflower",
-        "Cucumber",
-        "Papaya",
-        "Potato",
-        "Pumpkin",
-        "Radish",
-        "Tomato",
+            "Haricot",
+            "Melon amer",
+            "Calebasse",
+            "Brinjal",
+            "Brocoli",
+            "Chou",
+            "Poivron",
+            "Carotte",
+            "Chou-fleur",
+            "Concombre",
+            "Papaye",
+            "Pomme de terre",
+            "Citrouille",
+            "Radis blanc",
+            "Tomate",
         ]
 
         os.remove("image.jpg")
 
         return "This image most likely belongs to {} with a {:.2f} percent confidence.".format(
-        class_names[np.argmax(score)], 100 * np.max(score))
+            class_names[np.argmax(score)], 100 * np.max(score)
+        )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run()
